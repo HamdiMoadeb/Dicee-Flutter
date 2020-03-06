56e6116f8a8e4f697e9fb5ec29048a315caa4b41
Hamdi Moadeb <hamdimoadeb@gmail.com>
2020-03-06 11:21:38 +0100

    Adding random method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,21 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   int leftDiceNb = 4;
   int rightDiceNb = 1;
+
+  void changeDiceState() {
+    setState(() {
+      leftDiceNb = Random().nextInt(6) + 1;
+      rightDiceNb = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +42,7 @@ class DicePage extends StatelessWidget {
             child: FlatButton(
               onPressed: () {
                 print("Left Button Clicked");
+                changeDiceState();
               },
               child: Image.asset('images/dice$leftDiceNb.png'),
             ),
@@ -38,6 +52,7 @@ class DicePage extends StatelessWidget {
             child: FlatButton(
               onPressed: () {
                 print("Right Button Clicked");
+                changeDiceState();
               },
               child: Image.asset('images/dice$rightDiceNb.png'),
             ),
